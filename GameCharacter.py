8a8aa5bce955fd852226e2d3b54fc82eb3077977
katniis <154299918+katniis@users.noreply.github.com>
2025-05-09 18:08:09 +0800

    Update GameCharacter.py

--- GameCharacter.py
+++ GameCharacter.py
@@ -19,17 +19,17 @@ class GameCharacter(ABC):
         pass
 
 class Warrior(GameCharacter):
-    def __init__(self, health, mana, level = 1):
+    def __init__(self, level = 1):
         super().__init__(health = 150, mana = 30, level = level)
 
 class Mage(GameCharacter):
-    def __init__(self, health, mana, level=1):
+    def __init__(self, level=1):
         super().__init__(health = 80, mana = 120, level = level)
 
 class Archer(GameCharacter):
-    def __init__(self, health, mana, level=1):
+    def __init__(self, level=1):
         super().__init__(health = 100, mana = 60, level = level)
 
 class Assassin(GameCharacter):
-    def __init__(self, health, mana, level=1):
+    def __init__(self, level=1):
         super().__init__(health = 90, mana = 70, level = level)
