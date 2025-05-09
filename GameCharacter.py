1888a39e9dccbee1013d549ea4b1e1c2555f2e5c
ShinHank <marjolfalvendia@gmail.com>
2025-05-09 16:01:46 +0800

    parent class

--- GameCharacter.py
+++ GameCharacter.py
@@ -1,1 +1,21 @@
-print("hello world")
+from abc import ABC, abstractmethod
+
+class GameCharacter(ABC):
+    def __init__(self, health, mana, level):
+        self.health = 100
+        self.mana = 50
+        self.level = 1
+
+    @abstractmethod
+    def skill(self):
+        pass
+
+    @abstractmethod
+    def attack(self):
+        pass
+
+
+
+
+
+        
